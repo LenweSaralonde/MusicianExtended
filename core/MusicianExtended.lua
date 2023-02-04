c631d29014738c0caa695bcf10257564f47d69ce
LenweSaralonde <lenwe@lenwe.info>
2023-02-04 19:21:19 +0100

    Removed now unnecessary GetPlayerTooltipText() hook

--- core/MusicianExtended.lua
+++ core/MusicianExtended.lua
@@ -21,40 +21,6 @@ function Musician.Registry.GetVersionString()
 		"MusicianExtended=" .. GetAddOnMetadata("MusicianExtended", "Version")
 end
 
--- Add the MusicianExtended version in the player tooltip
---
-local hookedMusicianRegistryGetPlayerTooltipText = Musician.Registry.GetPlayerTooltipText
-function Musician.Registry.GetPlayerTooltipText(player)
-	local tooltipText = hookedMusicianRegistryGetPlayerTooltipText(player)
-
-	if tooltipText == nil then
-		return nil
-	end
-
-	-- Get player full version string, if any
-	player = Musician.Utils.NormalizePlayerName(player)
-
-	local versionString = Musician.Registry.players[player].version
-
-	if not versionString then
-		return tooltipText
-	end
-
-	-- Return tooltip text with Extended version, if found
-	local versionRows = { string.split(' ', versionString) }
-	for _, row in pairs(versionRows) do
-		local pluginName, pluginVersion = string.split('=', row)
-		if pluginName == "MusicianExtended" then
-			-- Only keep version-related characters to avoid escape sequences injection
-			pluginVersion = string.gsub(pluginVersion, "[^0-9a-zA-Z%.%-%_]+", "")
-			tooltipText = string.gsub(tooltipText, "Musician", "Musician: Extended")
-			return tooltipText .. " x " .. pluginVersion
-		end
-	end
-
-	return tooltipText
-end
-
 -- Enlarge required cache size
 --
 if Musician.Utils.GetSoundCacheSize then
